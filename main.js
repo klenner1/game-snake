4c6b5b7b78cf55208da519080fb8247cbcd69d41
klenner <klenne.al@gmail.com>
2020-12-19 18:41:20 -0300

    Updates

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("./index.scss");
-var game_1 = require("./Views/Game/game");
-var base = document.getElementById('base');
-var game = new game_1.Game(base);
-game.init();
