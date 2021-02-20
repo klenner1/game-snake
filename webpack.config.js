57e770744dbf5530584b71f50ad90e7a8a7204f6
klenner <klenne.al@gmail.com>
2021-02-20 12:38:14 -0300

    "start_url" in manifest by environment

--- webpack.config.js
+++ webpack.config.js
@@ -3,72 +3,82 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const loader = require("sass-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = {
-  entry: {
-    main: "./src/main.ts",
-    sw: "./src/service-worker.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: ["css-loader", "style-loader"],
-      },
-      {
-        test: /\.(js|ts)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
+module.exports = (env) => {
+  return {
+    entry: {
+      main: "./src/main.ts",
+      sw: "./src/service-worker.js",
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
-      },
-    ],
-  },
-  stats: {
-    colors: true,
-  },
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "src/Assets/", to: "assets" },
-        { from: "src/manifest.json" },
-        { from: "src/favicon.ico" },
+        {
+          test: /\.css$/,
+          use: ["css-loader", "style-loader"],
+        },
+        {
+          test: /\.(js|ts)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
       ],
-    }),
-  ],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  devServer: {
-    https: true,
-    key: fs.readFileSync('./path/server.key'),
-    cert: fs.readFileSync('./path/server.crt'),
-    //ca: fs.readFileSync('./path/ca.pem'),
-  }
+    },
+    stats: {
+      colors: true,
+    },
+    devtool: "source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "src/Assets/", to: "assets" },
+          {
+            from: "src/manifest.json",
+            transform: (content, path) => {
+              var manifest = JSON.parse(content.toString());
+              if (env.github) {
+                manifest.start_url = "/game-snake/";
+              }
+              return JSON.stringify(manifest);
+            },
+          },
+          { from: "src/favicon.ico" },
+        ],
+      }),
+    ],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    devServer: {
+      https: true,
+      key: fs.readFileSync("./path/server.key"),
+      cert: fs.readFileSync("./path/server.crt"),
+    },
+  };
 };
